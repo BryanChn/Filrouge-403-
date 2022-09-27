870c68369c5b9de42c00ed7291bae680cb7fdccd
Bryanlaptop <bryan.chaine@gmail.com>
2022-09-27 16:56:08 +0200

    screen home is done , navigation work

--- PandaFeedFront/App.tsx
+++ PandaFeedFront/App.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import Logo from './assets/PandaFEED.png';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/Screens/Home';
+import ShoppingList from './src/Features/ShoppingList/ShoppingList';
 
+export type AuthRootParamList = {
+  Home: undefined;
+  ShoppingList: undefined;
+};
+
+const Stack = createNativeStackNavigator<AuthRootParamList>();
 const App = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.root}>
-        <Image
-          style={[styles.Logo, {height: 1750 * 0.3}]}
-          resizeMode="contain"
-          source={Logo}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
         />
-      </View>
-    </SafeAreaView>
+        <Stack.Screen name="ShoppingList" component={ShoppingList} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-const styles = StyleSheet.create({
-  root: {
-    alignItems: 'center',
-    padding: 20,
-    flex: 1,
-    backgroundColor: '#8AC997',
-  },
 
-  Logo: {
-    width: '70%',
-    height: 175,
-    maxHeight: 200,
-  },
-});
 export default App;
---
+++ PandaFeedFront/src/Features/ShoppingList/ShoppingList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ShoppingList = () => {
+  return <View>HELLO</View>;
+};
+
+export default ShoppingList;
